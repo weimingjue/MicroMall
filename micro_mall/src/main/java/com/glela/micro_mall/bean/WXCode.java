0fb59f9aacbf89ab9c414538cd15d9629d3e80fd
“weimingjue” <873456775@qq.com>
2019-04-08 14:13:34 +0800

    微信、支付宝字段注释

--- micro_mall/src/main/java/com/glela/micro_mall/bean/WXCode.java
+++ micro_mall/src/main/java/com/glela/micro_mall/bean/WXCode.java
@@ -10,13 +10,12 @@ public class WXCode implements Serializable {
     }
 
     public String nonce_str;
-    public String paySign;
+    public String paySign;//对应sign
     public String appid;
-    public String sign;
     public String trade_type;
     public String return_msg;
     public String result_code;
-    public String mch_id;
+    public String mch_id;//对应partnerId
     public String return_code;
     public String prepay_id;
     public int timestamp;
